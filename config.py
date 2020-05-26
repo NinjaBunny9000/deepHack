f99e4272770d6fefb74e3d7b32b4971596a15c4e
Bun <ninjabunny9000@gmail.com>
2020-05-26 02:40:06 -0400

    config.yaml changed to secrets.yaml

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import yaml
 import os, sys
 
 # Load the main config file
-with open(os.path.join(sys.path[0], 'config.yaml'), "r") as f:
+with open(os.path.join(sys.path[0], 'secrets.yaml'), "r") as f:
     cfg = yaml.load(f, Loader=yaml.FullLoader)
 
 discord_token = cfg['discord']['token']
